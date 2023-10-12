a02898a472862dcbbb8f028ccaa1fe51d492bab9
Ramirez-05 <117038114+Ramirez-05@users.noreply.github.com>
2023-10-11 20:20:28 -0500

    Admin/vendedor 30%

--- Code/AppParqueadero/src/Alerts/AlertConfirmarDesligar.java
+++ Code/AppParqueadero/src/Alerts/AlertConfirmarDesligar.java
@@ -2,7 +2,7 @@
 package Alerts;
 
 import Main.ConsumoApi;
-import VistaParqueadero.DeleteParking;
+import VistaVendedor.Vendedores;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.awt.Dimension;
@@ -10,22 +10,23 @@ import java.awt.Toolkit;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AlertConfirmarDeleteParking extends javax.swing.JFrame {
+public class AlertConfirmarDesligar extends javax.swing.JFrame {
 
-    
-       
-    String nit;
+    private String cedula;
+    Vendedores contentVendedores;
+    ConsumoApi consumo;
     Gson gson;
-    DeleteParking delete;
     
-    public AlertConfirmarDeleteParking(String nit,DeleteParking delete) {
+    public AlertConfirmarDesligar(String cedula,Vendedores contentVendedores) {
+        this.cedula = cedula;
+        this.contentVendedores = contentVendedores;
+        consumo = new ConsumoApi();
         gson = new Gson();
-        this.nit = nit;
-        this.delete = delete;
         initComponents();
         centrarPantalla();
     }
 
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -34,8 +35,8 @@ public class AlertConfirmarDeleteParking extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        btnConfirmar = new javax.swing.JButton();
+        btnCancelar = new javax.swing.JButton();
+        btnDesligar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -49,30 +50,30 @@ public class AlertConfirmarDeleteParking extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(0, 0, 0));
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("UNA VEZ Al CONFIRMAR");
+        jLabel2.setText("UNA VEZ HECHOS LOS CAMBIOS");
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(0, 0, 0));
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setText("NO HAY VUELTA ATRAS.");
 
-        jButton1.setBackground(new java.awt.Color(255, 51, 51));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("CANCELAR");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnCancelar.setBackground(new java.awt.Color(255, 51, 51));
+        btnCancelar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnCancelar.setForeground(new java.awt.Color(255, 255, 255));
+        btnCancelar.setText("CANCELAR");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnCancelarActionPerformed(evt);
             }
         });
 
-        btnConfirmar.setBackground(new java.awt.Color(207, 191, 255));
-        btnConfirmar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        btnConfirmar.setForeground(new java.awt.Color(0, 0, 0));
-        btnConfirmar.setText("CONFIRMAR");
-        btnConfirmar.addActionListener(new java.awt.event.ActionListener() {
+        btnDesligar.setBackground(new java.awt.Color(207, 191, 255));
+        btnDesligar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnDesligar.setForeground(new java.awt.Color(0, 0, 0));
+        btnDesligar.setText("DESLIGAR");
+        btnDesligar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnConfirmarActionPerformed(evt);
+                btnDesligarActionPerformed(evt);
             }
         });
 
@@ -84,15 +85,15 @@ public class AlertConfirmarDeleteParking extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 356, Short.MAX_VALUE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(39, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(29, 29, 29)
-                .addComponent(btnConfirmar)
-                .addGap(42, 42, 42))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnCancelar)
+                .addGap(32, 32, 32)
+                .addComponent(btnDesligar)
+                .addGap(67, 67, 67))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -105,9 +106,9 @@ public class AlertConfirmarDeleteParking extends javax.swing.JFrame {
                 .addComponent(jLabel3)
                 .addGap(29, 29, 29)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnConfirmar)
-                    .addComponent(jButton1))
-                .addContainerGap(24, Short.MAX_VALUE))
+                    .addComponent(btnDesligar)
+                    .addComponent(btnCancelar))
+                .addContainerGap(29, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -130,62 +131,37 @@ public class AlertConfirmarDeleteParking extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        //CERRAMOS VENTANA DE ALERTA PARA CONFIRMAR UPDATE
         this.dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnCancelarActionPerformed
 
-    private void btnConfirmarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConfirmarActionPerformed
+    private void btnDesligarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDesligarActionPerformed
 
         //CODIGO PARA HACER EL UPDATE DEL PARQUEADERO
+        Map<String, String> queryDesligamiento = new HashMap<>();
+        queryDesligamiento.put("cedula",cedula);
 
-        ConsumoApi consumo = new ConsumoApi();
-        Map<String, String> insertData = new HashMap<>();
-        insertData.put("nit",nit);
+        String desligar = consumo.consumoPOST("http://localhost/APIenPHP/API-Personas/Desligar.php", queryDesligamiento);
 
-        String delete = consumo.consumoPOST("http://localhost/APIenPHP/API-parqueadero/Delete.php", insertData);
+        System.out.println("Lo que llego"+desligar);
 
-        System.out.println("Lo que llego"+delete);
-
-        JsonObject jsonResponse = gson.fromJson(delete, JsonObject.class);
+        JsonObject jsonResponse = gson.fromJson(desligar, JsonObject.class);
 
         boolean status = jsonResponse.get("status").getAsBoolean();
 
         if( status ){
-            
-            
-          //MOSTRAMOS VENTANA MAIN CON CONTENEDOR PARUQEADEROS
-            this.delete.contentParqueadero.main.setVisible(true);
-            this.delete.contentParqueadero.mostrarParqueaderos();
+
+            this.contentVendedores.mostrarVendedores();
             //MOSTRAMOS MENSAJE DE EXITO DE UPDATE
-            AlertDeleteExitoso mostrar = new AlertDeleteExitoso();
+            AlertExitoDesligar mostrar = new AlertExitoDesligar();
             mostrar.setVisible(true);
-            
-            
-            //CERRAMOS VENTANA DE DELETE
-            this.delete.dispose();
-            
-            
-            //CERRA VETANA ACTUAL
-            this.dispose();
-        }else{
-            
-            //MOSTRAMOS VENTANA MAIN CON CONTENEDOR PARUQEADEROS
-            this.delete.contentParqueadero.main.setVisible(true);
-            this.delete.contentParqueadero.mostrarParqueaderos();
-            //MOSTRAMOS MENSAJE DE EXITO DE UPDATE
-            AlertProblemDelete mostrar = new AlertProblemDelete();
-            mostrar.setVisible(true);
-            
-            
-            //CERRAMOS VENTANA DE DELETE
-            this.delete.dispose();
-            
-            
+
             //CERRA VETANA ACTUAL
             this.dispose();
         }
 
-    }//GEN-LAST:event_btnConfirmarActionPerformed
+    }//GEN-LAST:event_btnDesligarActionPerformed
 
     private void centrarPantalla(){
         // Centrar la ventana en la pantalla
@@ -196,9 +172,11 @@ public class AlertConfirmarDeleteParking extends javax.swing.JFrame {
         int frameHeight = getHeight();
         setLocation((screenWidth - frameWidth) / 2, (screenHeight - frameHeight) / 2);
     }
+   
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnConfirmar;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JButton btnDesligar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
