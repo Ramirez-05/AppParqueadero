5769ce085a125d871ed3c70b276b24f70a3c71a4
CG2850 <camilo.2850@hotmail.com>
2023-10-13 09:09:28 -0500

    Camilo

--- Code/AppParqueadero/src/Main/Entrada.java
+++ Code/AppParqueadero/src/Main/Entrada.java
@@ -1,7 +1,10 @@
 package Main;
+import Alerts.RegistroExitoso;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -29,10 +32,10 @@ public final class Entrada extends javax.swing.JPanel {
     private void initComponents() {
 
         nitParqueadero2 = new javax.swing.JLabel();
-        btnModificarTarifa = new javax.swing.JButton();
+        btnEntrada = new javax.swing.JButton();
         inputPlaca = new javax.swing.JTextField();
+        inputTitular = new javax.swing.JTextField();
         inputVehiculo = new javax.swing.JTextField();
-        inputVehiculo1 = new javax.swing.JTextField();
         jLabelPlaca = new javax.swing.JLabel();
         jLabelVehiculo = new javax.swing.JLabel();
         jLabelTitular = new javax.swing.JLabel();
@@ -41,13 +44,13 @@ public final class Entrada extends javax.swing.JPanel {
 
         setBackground(new java.awt.Color(255, 255, 255));
 
-        btnModificarTarifa.setBackground(new java.awt.Color(73, 59, 114));
-        btnModificarTarifa.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        btnModificarTarifa.setForeground(new java.awt.Color(255, 255, 255));
-        btnModificarTarifa.setText("INGRESAR");
-        btnModificarTarifa.addActionListener(new java.awt.event.ActionListener() {
+        btnEntrada.setBackground(new java.awt.Color(73, 59, 114));
+        btnEntrada.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnEntrada.setForeground(new java.awt.Color(255, 255, 255));
+        btnEntrada.setText("INGRESAR");
+        btnEntrada.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnModificarTarifaActionPerformed(evt);
+                btnEntradaActionPerformed(evt);
             }
         });
 
@@ -57,15 +60,15 @@ public final class Entrada extends javax.swing.JPanel {
             }
         });
 
-        inputVehiculo.addActionListener(new java.awt.event.ActionListener() {
+        inputTitular.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                inputVehiculoActionPerformed(evt);
+                inputTitularActionPerformed(evt);
             }
         });
 
-        inputVehiculo1.addActionListener(new java.awt.event.ActionListener() {
+        inputVehiculo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                inputVehiculo1ActionPerformed(evt);
+                inputVehiculoActionPerformed(evt);
             }
         });
 
@@ -93,11 +96,11 @@ public final class Entrada extends javax.swing.JPanel {
                         .addGap(34, 34, 34)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(inputPlaca, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(inputVehiculo1, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(inputVehiculo, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(inputVehiculo, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(inputTitular, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(btnModificarTarifa)
+                        .addComponent(btnEntrada)
                         .addGap(70, 70, 70)))
                 .addContainerGap(129, Short.MAX_VALUE))
         );
@@ -115,11 +118,11 @@ public final class Entrada extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(inputPlaca, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(28, 28, 28)
-                        .addComponent(inputVehiculo1, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(inputVehiculo, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(28, 28, 28)
-                        .addComponent(inputVehiculo, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(inputTitular, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(18, 18, 18)
-                .addComponent(btnModificarTarifa, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnEntrada, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(165, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -132,29 +135,65 @@ public final class Entrada extends javax.swing.JPanel {
     
     
     //Botón para editar parqueadero
-    private void btnModificarTarifaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModificarTarifaActionPerformed
-        System.out.println("SE APRETO EL BOTON MODIFICAR TARIFA");
-    }//GEN-LAST:event_btnModificarTarifaActionPerformed
+    private void btnEntradaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEntradaActionPerformed
+        ConsumoApi consumo = new ConsumoApi();
+        
+        String placa = inputPlaca.getText(); 
+        String vehiculo = inputVehiculo.getText(); 
+        String titular = inputTitular.getText();
+        
+        Map<String, String> postData = new HashMap<>();
+        postData.put("placa", placa);
+        postData.put("tipo_vehiculo", vehiculo);
+        postData.put("responsable", titular);
+        String insertTicket = consumo.consumoPOST("http://localhost/APIenPHP/API-ticket/insertTicket.php", postData);
+        System.out.println("respuesta api: "+ insertTicket);
+        if (insertTicket != null) {
+        JsonObject jsonTemp = gson.fromJson(insertTicket, JsonObject.class);
+        boolean status = jsonTemp.get("status").getAsBoolean();
+
+        // Verifica si "message" está presente en el JSON
+        if (jsonTemp.has("mesagge")) {
+            String mesagge = jsonTemp.get("mesagge").getAsString();
+
+
+            if (status) {
+                System.out.println("Correctamente.");
+                // Mostramos una alerta de que el parqueadero ya existe
+                    RegistroExitoso alert = new RegistroExitoso();
+                    alert.setVisible(true);
+            } else {
+                System.out.println("Error al actualizar la tarifa: " + mesagge);
+            }
+        } else {
+            System.out.println("Error: 'mesagge' no está presente en la respuesta JSON.");
+        }
+    } else {
+        System.out.println("Error al consumir la API.");
+    }
+
+                
+    }//GEN-LAST:event_btnEntradaActionPerformed
 
     private void inputPlacaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputPlacaActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_inputPlacaActionPerformed
 
+    private void inputTitularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputTitularActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inputTitularActionPerformed
+
     private void inputVehiculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputVehiculoActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_inputVehiculoActionPerformed
 
-    private void inputVehiculo1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputVehiculo1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_inputVehiculo1ActionPerformed
-
     
  
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnModificarTarifa;
+    private javax.swing.JButton btnEntrada;
     private javax.swing.JTextField inputPlaca;
+    private javax.swing.JTextField inputTitular;
     private javax.swing.JTextField inputVehiculo;
-    private javax.swing.JTextField inputVehiculo1;
     private javax.swing.JLabel jLabelPlaca;
     private javax.swing.JLabel jLabelTitular;
     private javax.swing.JLabel jLabelVehiculo;
